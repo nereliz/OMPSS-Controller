7f803589ce87dd4640b9f3fe30b49b0eaf3fd2cc
Nerijus Baraukas <nerijus@desktop.(none)>
2013-04-06 19:30:56 +0100

    [NF] Adding service scripts

---
+++ ompssc/ompssc.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+args=("$@");
+
+SCRIPTNAME="./OMPSSC"
+PIDFILE=/var/run/ompssc.pid
+cd /etc/ompss
+
+if [ "${args[0]}" == "start" ]
+  then
+	if [ -f ${PIDFILE} ]; then
+	   #verify if the process is actually still running under this pid
+	   OLDPID=`cat ${PIDFILE}`
+	   RESULT=`ps -ef | grep ${OLDPID} | grep ${SCRIPTNAME}`  
+
+	   if [ -n "${RESULT}" ]; then
+	     echo "Service already running! Exiting"
+	     exit 255
+	   else
+	      PID=`ps -ef | grep ${SCRIPTNAME} | grep -v sudo | grep -v color | head -n1 |  awk ' {print $2;} '`
+	      echo ${PID} > ${PIDFILE}
+	   fi
+ 	else
+	  sudo ./OMPSSC &
+	  sleep 2;
+	  PID=`ps -ef | grep ${SCRIPTNAME} | grep -v sudo | grep -v color | head -n1 |  awk ' {print $2;} '`
+	  echo ${PID} > ${PIDFILE}
+	fi
+elif [ "${args[0]}" == "stop" ]
+   then 
+	if [ -f ${PIDFILE} ]; then
+	   OLDPID=`cat ${PIDFILE}`
+	   RESULT=`ps -ef | grep ${OLDPID} | grep ${SCRIPTNAME}`  
+
+	   if [ -n "${RESULT}" ]; then
+	     echo "Service is stopping now!"
+	     `sudo kill -9 ${OLDPID}`
+	   else
+	      echo "Service was not found! Exiting"
+	   fi
+	   rm ${PIDFILE}
+	else
+	   echo "Service was not found! Exiting"
+	fi
+elif [ "${args[0]}" == "status" ]
+   then 
+	if [ -f ${PIDFILE} ]; then
+	   echo "Service is running!"
+	else
+	   echo "Service is stoped!"
+	fi
+fi
